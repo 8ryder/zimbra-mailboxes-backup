a6010d1e025bbce5723f228cb27bed9136242b7d
Volodymyr Ivaniuta <ivanyuta.vova@gmail.com>
2017-09-02 18:29:36 +0200

    Update zimbrabackup.sh

--- zimbrabackup.sh
+++ zimbrabackup.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-DESTINATION=/opt/zimbra/backup/mailbox/$(date '+%Y-%m-%d')
+DOMAIN=''
+DESTINATION=/opt/zimbra/backup/mailbox
+DESTINATION_PREFIX=zmbrbckp
+KEEP=5
 
-while [[ $# -gt 1 ]]
+while [[ $# -ge 1 ]]
 do
 key="$1"
 
 case $key in
     -d|--domain)
+        if [ -z $2 ]; then echo "domain option selected, but no domain inserted"
+        exit 1
+        fi
     DOMAIN="$2"
+    shift # past argument
+    ;;
+    -f|--folder)
+        if [ -z $2 ]; then
+                echo "folder option selected, but no path specified"
+                exit 1
+        fi
+
+        if ! [ -d "$2" ]; then
+                echo "the specified path is not valid"
+                exit 1
+        fi
+
+    DESTINATION="$2"
     shift # past argument
     ;;
     *)
@@ -18,24 +38,34 @@ esac
 shift # past argument or value
 done
 
+#set the real destination path, with selected path, prefix and today date
+DESTINATION_FINAL=$DESTINATION/$DESTINATION_PREFIX-$(date '+%Y-%m-%d')
+
 #keep only 5 folders with backups
-cd /opt/zimbra/backup/mailbox/
-rm -f $(ls -1t * | tail -n +6)
+cd $DESTINATION
+rm -f $(ls -1dt $DESTINATION/$DESTINATION_PREFIX-* | tail -n +$KEEP)
 
 #create the folder for the today backup
-mkdir /opt/zimbra/backup/mailbox/$(date '+%Y-%m-%d')
-
+mkdir $DESTINATION_FINAL
 
 
 echo ----
-echo $DOMAIN
+echo "selected domain " $DOMAIN
+echo "selected keeping number " $KEEP
+echo "selected destination folder " $DESTINATION
+echo "the final destination folder is: " $DESTINATION_FINAL
 echo ----
 
+#here we test if domain variable is set and then chose accounts to backup
+if [ -z $DOMAIN ]; then
+        ACCOUNTS=$(zmprov -l gaa)
+else
+        ACCOUNTS=$(zmprov -l gaa | grep $DOMAIN)
+fi
 
-
-echo $(zmprov -l gaa | grep $DOMAIN)
-        for i in $( zmprov -l gaa | grep $DOMAIN); do
-                echo "starting backup for" $i
-                /opt/zimbra/bin/zmmailbox -z -m $i -t 0 getRestURL '//?fmt=tgz' > $DESTINATION/$i.tgz
-                echo "finished backup for " $i
-       done
+for account in $ACCOUNTS;
+                do
+                        echo "starting backup for" $account
+                        /opt/zimbra/bin/zmmailbox -z -m $account -t 0 getRestURL '//?fmt=tgz' > $DESTINATION_FINAL/$account.tgz
+                        echo "finished backup for " $account
+                done
